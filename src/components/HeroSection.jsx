293f182a178fd04c21b2bd763ab5a5fab86b891d
Fortune <fortuneokechukwu206@gmail.com>
2025-04-28 12:32:45 +0100

    update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/HeroSection.css'; // create this CSS file
-
+import { FaWhatsapp } from 'react-icons/fa'; // WhatsApp icon
+import '../styles/HeroSection.css';
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
-        <div className="hero-content">
+      <div className="hero-content">
         <div className="overlay"></div>
         <h1>Unlock the adventure of a lifetime!</h1>
         <h3>From exotic beaches to bustling cities, we make your travel dreams come true</h3>
-        <Link to="/contact">    
-            <button className="hero-button">Book now</button>
-        </Link>
-        </div>
-    </div>
-  )
-}
 
-export default HeroSection
+        <div className="button-group">
+          <Link to="/contact">
+            <button className="hero-button">Book now</button>
+          </Link>
+
+          <a 
+            href="https://wa.me/2348116649112" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <button className="hero-button2">
+              <FaWhatsapp className="whatsapp-icon" />
+              Chat on WhatsApp
+            </button>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
